0402468b72ae94dbf3715229277328740073adb8
mt02-dev <t.m.215soccer@icloud.com>
2024-08-08 16:32:07 +0900

    routing　作成

---
+++ frontend/src/containers/Foods.tsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const Foods = ()  => {
+  
+  return (
+    <Fragment>
+      商品一覧
+      <p>{restaurantId}</p>
+    </Fragment>
+  )
+}
+
+export default Foods;
---
+++ frontend/src/containers/Restaurants.tsx
@@ -0,0 +1,14 @@
+import React, { Fragment } from "react";
+
+const Restaurants = () => {
+  return (
+    <Fragment>
+      レストラン一覧
+    </Fragment> 
+  ) 
+}
+
+export default Restaurants;
+
+
+
