d4437ab36b84ffc4c4ebf01bfe4d84e47c061aae
mt02-dev <t.m.215soccer@icloud.com>
2024-08-08 09:52:05 +0900

    model 作成

---
+++ app/models/restaurant.rb
@@ -0,0 +1,10 @@
+class Restaurants < ApplicationRecord
+  has_many :foods
+  # この関連付けによりそのレストランにある商品が仮注文されているかどうかを簡単に取得できる
+  has_many :line_foods, through: :foods
+
+  validates :name, :fee, :time_required, presence: true
+  validates :name, length: { maximum: 30}
+  validates :fee, numericality: { grater_than: 0 }
+
+end
